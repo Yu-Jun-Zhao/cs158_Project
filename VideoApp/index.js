b0a7ffe22cd949faffe146f0a386fe76123f4970
Yu Jun Zhao <yujun.zhao@sjsu.edu>
2018-05-06 16:06:09 -0700

    message

--- VideoApp/index.js
+++ VideoApp/index.js
@@ -1,1 +1,28 @@
-var Peer = require()
+var Peer = require('simple-peer');
+var peer = new Peer({
+
+  initiator: location.hash === '#init',
+  trickle: false
+});
+
+peer.on('signal', function(data) {
+  document.getElementById('YourConnectionID').value = JSON.stringify(data);
+
+
+})
+
+document.getElementById('connect').addEventListener('click', function(){
+  var otherId = JSON.parse(document.getElementById('OtherID').value)
+  peer.signal(otherId)
+
+})
+
+document.getElementById('send').addEventListener('click', function(){
+
+  var yourMessage = document.getElementById('yourMessage').value
+  peer.send(yourMessage)
+})
+
+peer.on('data', function(data){
+  document.getElementById('messages').textContent += data + '\n'
+})
