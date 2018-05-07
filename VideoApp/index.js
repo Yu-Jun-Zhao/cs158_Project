c426b5b5177953251d0af5561b1db922147ac3b5
Yu Jun Zhao <yujun.zhao@sjsu.edu>
2018-05-07 16:51:35 -0700

    signalmessage

--- VideoApp/index.js
+++ VideoApp/index.js
@@ -1,8 +1,30 @@
 const signalhub = require('signalhub');
-const hub = signalhub('my-video-app', ['http://localhost:8080']);
+const hub = signalhub('my-video-app', ['https://signalhub-jccqtwhdwc.now.sh']);
 
 
+hub.subscribe('update').on('data', function(data){
+    console.log(data);
+});
 
+document.getElementById('connect').addEventListener('click', function(){
+    hub.broadcast('update', window.location.hash);
+});
+
+
+document.getElementById('send').addEventListener('click', function(){
+
+  var yourMessage = document.getElementById('yourMessage').value;
+  hub.broadcast('update', yourMessage);
+  document.getElementById('messages').textContent += 'you ' + '>> ' +  yourMessage + '\n';
+
+})
+
+//setInterval(function(){
+    //document.getElementById('messages').textContent += '    ' +
+    //console.log("");
+//}, 1000);
+
+/*
 navigator.mediaDevices.getUserMedia({video: true, audio: false})
   .then(function (stream) {
     var Peer = require('simple-peer');
@@ -56,53 +78,4 @@ navigator.mediaDevices.getUserMedia({video: true, audio: false})
   });
 
 
-/*
-var getUserMedia = require('getusermedia');
-
-getUserMedia({video: true, audio: false}, function (err, stream) {
-    // if the browser doesn't support user media
-    // or the user says "no" the error gets passed
-    // as the first argument.
-    if (err) {
-       console.log('failed' + err);
-    } else {
-       console.log('got a stream', stream);
-    }
-    var Peer = require('simple-peer');
-    var peer = new Peer({
-
-      initiator: location.hash === '#init',
-      trickle: false,
-      stream: stream
-    });
-
-    peer.on('signal', function(data) {
-      document.getElementById('YourConnectionID').value = JSON.stringify(data);
-    })
-
-    document.getElementById('connect').addEventListener('click', function(){
-      var otherId = JSON.parse(document.getElementById('OtherID').value)
-      peer.signal(otherId)
-
-    })
-
-    document.getElementById('send').addEventListener('click', function(){
-
-      var yourMessage = document.getElementById('yourMessage').value
-      peer.send(yourMessage)
-    })
-
-    peer.on('data', function(data){
-      document.getElementById('messages').textContent += data + '\n'
-    })
-
-    peer.on('stream',function(stream){
-      var video = document.createElement('video')
-      document.body.appendChild(video)
-
-      video.src = window.URL.createObjectURL(stream)
-      video.play()
-    })
-  }
-);
 */
