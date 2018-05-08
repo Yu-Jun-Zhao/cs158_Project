113e1b2d1ecf80c33c65dc9ce50336395aefb8cb
Yu Jun Zhao <yujun.zhao@sjsu.edu>
2018-05-07 22:41:16 -0700

    Finish?

--- VideoApp/index.js
+++ VideoApp/index.js
@@ -1,6 +1,5 @@
 const signalhub = require('signalhub');
 const hub = signalhub('my-video-app', ['https://signalhub-jccqtwhdwc.now.sh']);
-//const Peer = require('./Peer.js');
 
 function Peer(name, message){
     this.name = name || "anonymous";
@@ -55,59 +54,3 @@ document.getElementById('send').addEventListener('click', function(){
     //document.getElementById('messages').textContent += '    ' +
     //console.log("");
 //}, 1000);
-
-/*
-navigator.mediaDevices.getUserMedia({video: true, audio: false})
-  .then(function (stream) {
-    var Peer = require('simple-peer');
-    var peer = new Peer({
-
-      initiator: location.hash === '#init',
-      trickle: false,
-      stream: stream
-    });
-
-    peer.on('signal', function(data) {
-      console.log(location.hash + " is here");
-      console.log(data);
-      document.getElementById('YourConnectionID').value = JSON.stringify(data);
-    })
-
-    document.getElementById('connect').addEventListener('click', function(){
-      var otherId = JSON.parse(document.getElementById('OtherID').value)
-      if(otherId.value === ""){
-        document.getElementById('statuslabel').innerHTML = "No Value in OTHER ID";
-      }else{
-        document.getElementById('statuslabel').innerHTML = "Connecting";
-        peer.signal(otherId);
-      }
-
-    })
-
-    document.getElementById('send').addEventListener('click', function(){
-
-      var yourMessage = document.getElementById('yourMessage').value
-      peer.send(yourMessage)
-    })
-
-    peer.on('data', function(data){
-      document.getElementById('messages').textContent += data + '\n'
-    })
-
-    peer.on('stream',function(stream){
-      var video = document.createElement('video')
-      console.log(location.hash)
-      document.body.appendChild(video)
-
-      video.src = window.URL.createObjectURL(stream)
-      video.play()
-    })
-  })
-  .catch(function (error) {
-    if (error) {
-       console.log('failed' + error);
-    }
-  });
-
-
-*/
